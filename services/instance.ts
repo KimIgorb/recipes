4998778b93bbbf0e1e14a1cf1f267b3a051bbf8b
Игорь <2712190@gmail.com>
2025-01-05 20:56:55 +0500

    test

--- services/instance.ts
+++ services/instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const primaryAxios = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_RECIPES_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_RECIPES_API_URL || 'https://dummyjson.com/',
 })
 
 export const secondaryAxios = axios.create({
